e9c7dd0c3b412085750b70b8347a21e953a4d647
Osasu Eboh <osawill@gmail.com>
2017-02-05 20:27:18 -0500

    Removed database key, store privately

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+var secrets = require('./secret');
+
 module.exports = {
-	db: 'postgres://utwctwvtlrpkme:pcX5zqzOnHtf3AW_ZnWbqp0LmO@ec2-107-20-242-191.compute-1.amazonaws.com:5432/d97omc7gv1shl?ssl=true',
+	db: secrets.db,
 	
 }
